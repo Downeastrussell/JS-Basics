531a459c28b79720052fc6cb1b2b2d5d587b4490
Russell Miller <bet2buy@gmail.com>
2018-12-06 20:02:04 -0500

    JS Basics

---
+++ script.js
@@ -0,0 +1,41 @@
+var firstName = "Russell";
+var lastName = "Miller";
+var age = 32;
+likesTravel = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(likesTravel);
+
+var movieArray = ['The Matrix', 'Boondock Saints', 'Toy Story'];
+
+console.log(movieArray[0]);
+console.log(movieArray[1]);
+console.log(movieArray[2]);
+
+var williamGrades = [62, 97, 99, 85, 73, 97];
+
+console.log[0];
+console.log[5];
+
+var averageGrade = (williamGrades[0] + williamGrades[1] + williamGrades[2] +williamGrades[3] + williamGrades[4] + williamGrades[5])/williamGrades.length;
+
+console.log(averageGrade);
+
+var movieStar = {
+    firstName: 'Russell',
+    lastName: 'Miller',
+    age: 32,
+    favoriteFood: 'Steak',
+    hobbies: ['Snowboarding', 'Wakeboarding', 'Guns', 'Hiking and Camping', 'Bitcoin', 'video games' ]
+}
+
+console.log(movieStar.favoriteFood);
+var fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+console.log(fullName + " likes to spend time watching cats play even though he is " + age + "." + " He also loves to eat " + movieStar.favoriteFood + " while watching said cats");
+
+
+
